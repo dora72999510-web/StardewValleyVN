9e60b9f06f6cff153537138c9904dbf047be9e8f
dora72999510-web <dora72999510@gmail.com>
2026-06-26 08:31:14 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "",
+        name: "Stardew Valley",
         // Activity type number (0 = Playing).
         type: 0,
       },
