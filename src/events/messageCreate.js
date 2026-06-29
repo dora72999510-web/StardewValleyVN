55b349166b90e4e26367ba493b50da02f919f53d
dora72999510-web <dora72999510@gmail.com>
2026-06-29 12:56:36 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -139,7 +139,7 @@ async function handleProtectedChannels(message) {
 
     if (logChannel?.isTextBased()) {
       await logChannel.send(
-        `🚫 ${member} bị timeout trong <#${message.channel.id}>`
+        `🚫 ${member.user.tag} bị timeout trong #${message.channel.name}`
       );
     }
 
