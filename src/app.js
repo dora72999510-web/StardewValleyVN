9b9a5acecd000a516acbfafa018b07979728354d
dora72999510-web <dora72999510@gmail.com>
2026-08-14 00:53:41 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -466,7 +466,7 @@ class TitanBot extends Client {
          */
 
         name:
-          'phat trien boi (developed by)',
+          'nha phat trien',
 
         /*
          * Đây là phần mô tả hiển thị bên dưới command.
