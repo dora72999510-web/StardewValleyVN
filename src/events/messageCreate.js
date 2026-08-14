26c6d27b479bc6ad3d24969f4edaa4fe86fbe26a
dora72999510-web <dora72999510@gmail.com>
2026-08-14 20:27:39 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -413,7 +413,7 @@ async function handleProtectedChannels(message) {
   const embed = new EmbedBuilder()
     .setColor(0xf1c40f)
     .setDescription(
-      `<:emoji_134:1523413261574471701> Tài khoản ${member} đã bị hạn chế 1 ngày ` +
+      `Tài khoản ${member} đã bị hạn chế 1 ngày ` +
       `do gửi nội dung vào <#1521007503263928341>`
     );
 
